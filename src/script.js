779c7779e552a01f4e795b10947e66d2114cbd75
stephdeandrade <stephdeandrade21@gmail.com>
2023-03-16 17:49:10 +0100

    children

--- src/script.js
+++ src/script.js
@@ -16,29 +16,57 @@ window.addEventListener('mousemove', (event) => {
 // Scene
 const scene = new THREE.Scene()
 
-// Red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// // Red cube
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+
+const plane = new THREE.PlaneGeometry(100,100);
+const planemat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    side : THREE.DoubleSide,
+});
+
+const floor = new THREE.Mesh(plane, planemat);
+scene.add(floor);
+floor.position.z = -2
+floor.receiveShadow = true;
+
 
 // // Models
-// const gltfloader = new GLTFLoader()
-// gltfloader.load(
-//     '/models/scene.gltf',
-//     (gltf) => 
-//     {
-//         scene.add(gltf.scene.children[1])
-//     }
-// )
+
+const loader = new GLTFLoader();
+
+
+loader.load( 'models/burger.glb', function ( gltf ) {
+    gltf.scene.scale.set(3,3,3)
+    scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+    console.error( error );
+    
+} );
+
+
+
+
 
 // Lights
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-// scene.add(ambientLight)
+ const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+ scene.add(ambientLight)
+
+   const sunLight = new THREE.DirectionalLight("#ffffff", 2);
+    sunLight.castShadow = true;
+    sunLight.shadow.camera.far = 20;
+    sunLight.shadow.mapSize.set(2048,2048);
+    sunLight.shadow.normalBias = 0.05;
+    sunLight.position.set(-1.5, 7, 3);
+   scene.add(sunLight);
 
 
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
 
 
 // Sizes
@@ -82,13 +110,21 @@ window.addEventListener('dblclick', () => {
 
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-camera.lookAt(mesh.position)
-scene.add(camera)
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera( (-aspectRatio * 5)/2,
+(aspectRatio * 5)/2,
+5/2,
+-5/2,
+-50,
+50 );
+scene.add( camera );
+
+
+
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
+//  const controls = new OrbitControls(camera, canvas)
 
 
 // Renderer
@@ -98,6 +134,13 @@ const controls = new OrbitControls(camera, canvas)
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.CineonToneMapping;
+renderer.toneMappingExposure = 1.75;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 
 // Animate
 const clock = new THREE.Clock()
@@ -105,7 +148,7 @@ const clock = new THREE.Clock()
 const tick = () => {
     const elapsedtime = clock.getElapsedTime()
 
-    controls.update()
+    // controls.update()
 
     renderer.render(scene, camera)
     
